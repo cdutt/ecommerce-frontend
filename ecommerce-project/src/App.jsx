5ea2bd7bba98720af4cc39d573321337ea638f15
Charudutt <gcharudutt@gmail.com>
2026-08-29 19:46:18 +0530

    checkout folder made and changed name of link tags from a to link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
-import { CheckOutPage } from "./pages/CheckoutPage";
+import { CheckOutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import "./App.css";
---
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,11 @@
+import "./checkoutPage.css";
+import { CheckoutHeader } from "./CheckoutHeader";
+
+export function CheckOutPage() {
+  return (
+    <>
+      <title>Checkout</title>
+      <CheckoutHeader />
+    </>
+  );
+}
