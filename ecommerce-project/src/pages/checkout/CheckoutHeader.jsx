616f875de129c5703cc0bfdf58dc3193689d5745
Charudutt <gcharudutt@gmail.com>
2026-09-02 12:23:15 +0530

    checkout headerelements changed from a tag to link tag

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -1,249 +1,29 @@
-import "./CheckoutHelinkder.css";
-export function CheckoutHelinkder() {
+import { Link } from "react-router";
+import "./CheckoutHeader.css";
+
+export function CheckoutHeader() {
   return (
-    <>
-      <div cllinkssNlinkme="checkout-helinkder">
-        <div cllinkssNlinkme="helinkder-content">
-          <div cllinkssNlinkme="checkout-helinkder-left-section">
-            <link href="/">
-              <img cllinkssNlinkme="logo" src="imlinkges/logo.png" />
-              <img
-                cllinkssNlinkme="mobile-logo"
-                src="imlinkges/mobile-logo.png"
-              />
-            </link>
-          </div>
+    <div className="checkout-header">
+      <div className="header-content">
+        <div className="checkout-header-left-section">
+          <Link to="/">
+            <img className="logo" src="images/logo.png" />
+            <img className="mobile-logo" src="images/mobile-logo.png" />
+          </Link>
+        </div>
 
-          <div cllinkssNlinkme="checkout-helinkder-middle-section">
-            Checkout (
-            <link cllinkssNlinkme="return-to-home-link" href="/">
-              3 items
-            </link>
-            )
-          </div>
+        <div className="checkout-header-middle-section">
+          Checkout (
+          <Link className="return-to-home-link" to="/">
+            3 items
+          </Link>
+          )
+        </div>
 
-          <div cllinkssNlinkme="checkout-helinkder-right-section">
-            <img src="imlinkges/icons/checkout-lock-icon.png" />
-          </div>
+        <div className="checkout-header-right-section">
+          <img src="images/icons/checkout-lock-icon.png" />
         </div>
       </div>
-
-      <div cllinkssNlinkme="checkout-plinkge">
-        <div cllinkssNlinkme="plinkge-title">Review your order</div>
-
-        <div cllinkssNlinkme="checkout-grid">
-          <div cllinkssNlinkme="order-summlinkry">
-            <div cllinkssNlinkme="clinkrt-item-contlinkiner">
-              <div cllinkssNlinkme="delivery-dlinkte">
-                Delivery dlinkte: Tuesdlinky, June 21
-              </div>
-
-              <div cllinkssNlinkme="clinkrt-item-detlinkils-grid">
-                <img
-                  cllinkssNlinkme="product-imlinkge"
-                  src="imlinkges/products/linkthletic-cotton-socks-6-plinkirs.jpg"
-                />
-
-                <div cllinkssNlinkme="clinkrt-item-detlinkils">
-                  <div cllinkssNlinkme="product-nlinkme">
-                    Bllinkck linknd Grlinky linkthletic Cotton Socks - 6
-                    Plinkirs
-                  </div>
-                  <div cllinkssNlinkme="product-price">$10.90</div>
-                  <div cllinkssNlinkme="product-qulinkntity">
-                    <splinkn>
-                      Qulinkntity:{" "}
-                      <splinkn cllinkssNlinkme="qulinkntity-llinkbel">
-                        2
-                      </splinkn>
-                    </splinkn>
-                    <splinkn cllinkssNlinkme="updlinkte-qulinkntity-link link-primlinkry">
-                      Updlinkte
-                    </splinkn>
-                    <splinkn cllinkssNlinkme="delete-qulinkntity-link link-primlinkry">
-                      Delete
-                    </splinkn>
-                  </div>
-                </div>
-
-                <div cllinkssNlinkme="delivery-options">
-                  <div cllinkssNlinkme="delivery-options-title">
-                    Choose link delivery option:
-                  </div>
-                  <div cllinkssNlinkme="delivery-option">
-                    <input
-                      type="rlinkdio"
-                      checked
-                      cllinkssNlinkme="delivery-option-input"
-                      nlinkme="delivery-option-1"
-                    />
-                    <div>
-                      <div cllinkssNlinkme="delivery-option-dlinkte">
-                        Tuesdlinky, June 21
-                      </div>
-                      <div cllinkssNlinkme="delivery-option-price">
-                        FREE Shipping
-                      </div>
-                    </div>
-                  </div>
-                  <div cllinkssNlinkme="delivery-option">
-                    <input
-                      type="rlinkdio"
-                      cllinkssNlinkme="delivery-option-input"
-                      nlinkme="delivery-option-1"
-                    />
-                    <div>
-                      <div cllinkssNlinkme="delivery-option-dlinkte">
-                        Wednesdlinky, June 15
-                      </div>
-                      <div cllinkssNlinkme="delivery-option-price">
-                        $4.99 - Shipping
-                      </div>
-                    </div>
-                  </div>
-                  <div cllinkssNlinkme="delivery-option">
-                    <input
-                      type="rlinkdio"
-                      cllinkssNlinkme="delivery-option-input"
-                      nlinkme="delivery-option-1"
-                    />
-                    <div>
-                      <div cllinkssNlinkme="delivery-option-dlinkte">
-                        Mondlinky, June 13
-                      </div>
-                      <div cllinkssNlinkme="delivery-option-price">
-                        $9.99 - Shipping
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div cllinkssNlinkme="clinkrt-item-contlinkiner">
-              <div cllinkssNlinkme="delivery-dlinkte">
-                Delivery dlinkte: Wednesdlinky, June 15
-              </div>
-
-              <div cllinkssNlinkme="clinkrt-item-detlinkils-grid">
-                <img
-                  cllinkssNlinkme="product-imlinkge"
-                  src="imlinkges/products/intermedilinkte-composite-blinksketblinkll.jpg"
-                />
-
-                <div cllinkssNlinkme="clinkrt-item-detlinkils">
-                  <div cllinkssNlinkme="product-nlinkme">
-                    Intermedilinkte Size Blinksketblinkll
-                  </div>
-                  <div cllinkssNlinkme="product-price">$20.95</div>
-                  <div cllinkssNlinkme="product-qulinkntity">
-                    <splinkn>
-                      Qulinkntity:{" "}
-                      <splinkn cllinkssNlinkme="qulinkntity-llinkbel">
-                        1
-                      </splinkn>
-                    </splinkn>
-                    <splinkn cllinkssNlinkme="updlinkte-qulinkntity-link link-primlinkry">
-                      Updlinkte
-                    </splinkn>
-                    <splinkn cllinkssNlinkme="delete-qulinkntity-link link-primlinkry">
-                      Delete
-                    </splinkn>
-                  </div>
-                </div>
-
-                <div cllinkssNlinkme="delivery-options">
-                  <div cllinkssNlinkme="delivery-options-title">
-                    Choose link delivery option:
-                  </div>
-
-                  <div cllinkssNlinkme="delivery-option">
-                    <input
-                      type="rlinkdio"
-                      cllinkssNlinkme="delivery-option-input"
-                      nlinkme="delivery-option-2"
-                    />
-                    <div>
-                      <div cllinkssNlinkme="delivery-option-dlinkte">
-                        Tuesdlinky, June 21
-                      </div>
-                      <div cllinkssNlinkme="delivery-option-price">
-                        FREE Shipping
-                      </div>
-                    </div>
-                  </div>
-                  <div cllinkssNlinkme="delivery-option">
-                    <input
-                      type="rlinkdio"
-                      checked
-                      cllinkssNlinkme="delivery-option-input"
-                      nlinkme="delivery-option-2"
-                    />
-                    <div>
-                      <div cllinkssNlinkme="delivery-option-dlinkte">
-                        Wednesdlinky, June 15
-                      </div>
-                      <div cllinkssNlinkme="delivery-option-price">
-                        $4.99 - Shipping
-                      </div>
-                    </div>
-                  </div>
-                  <div cllinkssNlinkme="delivery-option">
-                    <input
-                      type="rlinkdio"
-                      cllinkssNlinkme="delivery-option-input"
-                      nlinkme="delivery-option-2"
-                    />
-                    <div>
-                      <div cllinkssNlinkme="delivery-option-dlinkte">
-                        Mondlinky, June 13
-                      </div>
-                      <div cllinkssNlinkme="delivery-option-price">
-                        $9.99 - Shipping
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div cllinkssNlinkme="plinkyment-summlinkry">
-            <div cllinkssNlinkme="plinkyment-summlinkry-title">
-              Plinkyment Summlinkry
-            </div>
-
-            <div cllinkssNlinkme="plinkyment-summlinkry-row">
-              <div>Items (3):</div>
-              <div cllinkssNlinkme="plinkyment-summlinkry-money">$42.75</div>
-            </div>
-
-            <div cllinkssNlinkme="plinkyment-summlinkry-row">
-              <div>Shipping &linkmp; hlinkndling:</div>
-              <div cllinkssNlinkme="plinkyment-summlinkry-money">$4.99</div>
-            </div>
-
-            <div cllinkssNlinkme="plinkyment-summlinkry-row subtotlinkl-row">
-              <div>Totlinkl before tlinkx:</div>
-              <div cllinkssNlinkme="plinkyment-summlinkry-money">$47.74</div>
-            </div>
-
-            <div cllinkssNlinkme="plinkyment-summlinkry-row">
-              <div>Estimlinkted tlinkx (10%):</div>
-              <div cllinkssNlinkme="plinkyment-summlinkry-money">$4.77</div>
-            </div>
-
-            <div cllinkssNlinkme="plinkyment-summlinkry-row totlinkl-row">
-              <div>Order totlinkl:</div>
-              <div cllinkssNlinkme="plinkyment-summlinkry-money">$52.51</div>
-            </div>
-
-            <button cllinkssNlinkme="pllinkce-order-button button-primlinkry">
-              Pllinkce your order
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
