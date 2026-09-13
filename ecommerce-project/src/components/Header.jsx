f11e2e6784f3dd585a64059c219fe39a82ac4e3c
Charudutt <gcharudutt@gmail.com>
2026-09-14 03:54:21 +0530

    fixed small h to H in header jsx file at import of Header css

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -3,7 +3,7 @@ import CartIcon from "../assets/images/icons/cart-icon.png";
 import SearchIcon from "../assets/images/icons/search-icon.png";
 import LogoWhite from "../assets/images/logo-white.png";
 import MobileLogoWhite from "../assets/images/mobile-logo-white.png";
-import "./header.css";
+import "./Header.css";
 import { useState } from "react";
 
 import { Link, useNavigate, useSearchParams } from "react-router";
